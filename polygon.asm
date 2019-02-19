af1b4ea334a96d46bdd0e2827232427a99da9529
Micrified <charles.xrandolph2@gmail.com>
2019-02-19 23:19:03 +0100

    Fixed polygon.asm but still encountering compile errors

--- polygon.asm
+++ polygon.asm
@@ -1,10 +1,11 @@
-	segment .data
+	section .data
 sfmt	db		"%ld", 0x0
 ifmt	db		"%lf%lf", 0x0
+pfmt	db		"Area = %lf", 0xa, 0x0
 efmt	db		"Err: Either bad input, or malloc failed!", 0xa, 0x0
 mptr	dq		0x0
 
-	segment .text
+	section .text
 	global main
 	extern scanf, printf, malloc, free
 	default rel
@@ -25,10 +26,14 @@ area:
 	; Set counter to zero
 	xor rbx, rbx
 
+	; Set sum to zero.
+	xorps xmm0, xmm0
+
 	; xmm0 = cumulative sum
 	; xmm1 = multiply #1
 	; xmm2 = multiply #2
 	; xmm3 = temporary sum
+
 .start:
 	mov rax, [rsp+.n]
 	cmp rbx, rax
@@ -38,31 +43,42 @@ area:
 	mov rcx, rbx
 	imul rcx, 0x8
 
-	; Move [x[n], x[n+1]] into xmm1
+	; Move x[n] into lower-order bits of xmm1
 	mov rsi, [rsp+.xs]
 	add rsi, rcx
-	movapd xmm1, [rsi]
+	movsd xmm1, [rsi]
 
-	; Move [y[n+1], y[n]] into xmm2
+	; Move x[n+1] into high-order bits of xmm1
+	movhpd xmm1, [rsi+8]
+
+	; Move y[n+1] into lower-order bits of xmm2
 	mov rsi, [rsp+.ys]
 	add rsi, rcx
-	movhpd xmm2, [rsi]
-	movlpd xmm2, [rsi+8]
-
-	; Multiply the values.
-	mulpd xmm1, xmm2
+	movsd xmm2, [rsi+8]
 	
-	; Add them in xmm1
-	movsd xmm3, xmm1
-	pshldq xmm1, 8
-	addsd xmm3, xmm1
+	; Move y[n] into higher-order bits of xmm2
+	movhpd xmm2, [rsi]
 
-	; Add to cumulative sum.
+	; Multiply xmm2 with xmm1 in a packed mode.
+	mulpd xmm2, xmm1
+
+	; Move lower-order double to xmm3
+	movsd xmm3, xmm2
+
+	; Shift xmm2 to put higher-order value in lower-order position.
+	pshldq xmm2, 8
+
+	; Add lower-order double to xmm3
+	addsd xmm3, xmm2
+
+	; Add xmm3 to cumulative sum in xmm0
 	addsd xmm0, xmm3
 
 	inc rbx
 	jmp .start
+
 .end:
+	xor rax, rax
 	leave
 	ret
 
@@ -103,23 +119,22 @@ fill:
 	jmp .start
 .end:
 
-	; Compute last position offset.
-	mov rbx, [rsp+.n]					; N
-	dec rbx								; (N-1)
-	imul rbx, 0x8						; (N-1) * sizeof(double)
+	; Get offset for last assignable position (N)
+	mov rcx, [rsp+.n]
+	imul rcx, 0x8						; Compute size in bytes.
 
-	; Copy x[0] to x[n-1]
+	; Copy x[0] to x[n]
 	mov rax, [rsp+.xs]					; Get pointer to array head
 	mov rsi, [rax]						; Extract first value.
 	mov rdi, [rsp+.xs]					; Get pointer again.
-	add rdi, rbx						; Add offset to pointer.
+	add rdi, rcx						; Add offset to pointer.
 	mov [rdi], rsi						; Save value at pointer.
 
-	; Copy y[0] to y[n-1]
+	; Copy y[0] to y[n]
 	mov rax, [rsp+.ys]					; Get pointer to array head
 	mov rsi, [rax]						; Extract first value.
 	mov rdi, [rsp+.ys]					; Get pointer again.
-	add rdi, rbx						; Add offset to pointer.
+	add rdi, rcx						; Add offset to pointer.
 	mov [rdi], rsi						; Save value at pointer.
 	
 	xor rax, rax
@@ -148,30 +163,31 @@ main:
 	cmp rax, 0x0
 	jle .end
 
-	; Increment N by 1 for a last position.
-	mov rax, [rsp+.n]
-	inc rax
-	mov [rsp+.n], rax
-
 	; Call malloc.
-	mov rdi, [rsp+.n]		; Load length.
-	shl rdi, 1				; Double it for size of both arrays.
-	imul rdi, 0x8			; Multiply by 8 to get size in bytes.
-	call malloc				; Allocate said memory.
-	mov [mptr], rax			; Save the pointer.
+	mov rdi, [rsp+.n]		; Load length (N)
+	inc rdi					; Increment to get (N+1)
+	shl rdi, 1				; Double to get (2*(N+1)) which is how many doubles we need total.
+	imul rdi, 0x8			; Multiply by sizeof(double) ~ 8 bytes.
+	call malloc				; Allocate the memory.
 
-	; Set xs pointer.
+	; Checking malloc output.
+	cmp rax, 0x0
+	jz .err					; If result is a null pointer, then abort.
+	mov [mptr], rax			; Otherwise save the pointer.
+
+	; Assign the xs pointer.
 	mov rsi, [mptr]
 	mov [rsp+.xs], rsi
 
-	; Set ys pointer.
+	; Assign the ys pointer.
 	mov rsi, [mptr]
 	mov rax, [rsp+.n]		; Load the length
+	inc rax					; Increment to get (N+1) which is the actual length.
 	imul rax, 0x8			; Multiply by 8 to get offset in bytes.
 	add rsi, rax			; Add the offset to compute the pointer.
 	mov [rsp+.ys], rsi		; Save the pointer.
 
-	; Fill both arrays.
+	; Get input for both arrays.
 	mov rdi, [rsp+.n]
 	mov rsi, [rsp+.xs]
 	mov rdx, [rsp+.ys]
@@ -179,13 +195,14 @@ main:
 
 	; Compute the area.
 	mov rdi, [rsp+.n]		; Load the length
-	shr rdi, 0x1			; Divide by 2 to get individual length
 	mov rsi, [rsp+.xs]		; Load the x coordinate array pointer.
 	mov rdx, [rsp+.ys]		; Load the y coordinate array pointer.
 	call area				; Call area.
 
-	; Print the area
-	; .. todo
+	; Print the area, value is in xmm0.
+	lea rdi, [pfmt]
+	mov eax, 0x1			; Set number of floating point args.
+	call printf
 
 	; Free the allocated memory.
 	mov rdi, [mptr]
